6ff5c81abb4323e6b1a60f951747b56f76407c36
zardron <zardron.pesquera1993@gmail.com>
2024-01-25 15:23:05 +0400

    creating UI form and create a function to connect on the backend server to get posts

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -1,7 +1,6 @@
 const reducers = (posts = [], action) => {
   switch (action.type) {
     case "FETCH_ALL":
-      console.log(action.payload);
       return action.payload;
     case "CREATE":
       return posts;
