a0b831db555b5a225caf8e5ca53786be24a03e4c
Zardron <zardron@clockworkdelivery.com>
2024-01-25 20:13:31 +0400

    creating post UI

--- server/controllers/postController.js
+++ server/controllers/postController.js
@@ -4,8 +4,6 @@ export const getPost = async (req, res) => {
   try {
     const post = await Post.find();
 
-    console.log(post);
-
     res.status(200).json(post);
   } catch (error) {
     res.status(404).json(error.message);
