d30f4d2fb638677a12b4d3442a85fe731ed147e5
zardron <zardron.pesquera1993@gmail.com>
2024-01-25 12:50:34 +0400

    setting up database connection

--- server/index.js
+++ server/index.js
@@ -2,3 +2,19 @@ import express from "express";
 import bodyParser from "body-parser";
 import mongoose from "mongoose";
 import cors from "cors";
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(bodyParser.json({ limit: "30mb", extended: true }));
+app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+mongoose.connect(process.env.MONGODB_URI).then(() => {
+  app.listen(process.env.PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`);
+  });
+});
